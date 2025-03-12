159fbe5fc91ab1b39c960c72c7da19d4b251fa00
Kathleen 陳宜伶 <144753651+B1229013@users.noreply.github.com>
2025-03-12 15:28:01 +0800

    Add files via upload

---
+++ lab02/hello_django/myapp/models.py
@@ -0,0 +1,7 @@
+from django.db import models # type: ignore
+class Post(models.Model):
+        title=models.CharField(max_length=100)
+        content = models.TextField(blank=True)
+        photo =models.URLField(blank=True)
+        location = models.CharField(max_length=100)
+        created_at = models.DateTimeField(auto_now_add=True)
